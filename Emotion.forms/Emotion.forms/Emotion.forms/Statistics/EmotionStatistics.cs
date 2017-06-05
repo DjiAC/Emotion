e5b86b1169e2e8ae40fdab9891d50364d82f4502
Adrien Ceccaldi <adrien.ceccaldi@hotmail.fr>
2017-06-05 23:39:09 +0200

    Start adding Statistics

---
+++ Emotion.forms/Emotion.forms/Emotion.forms/Statistics/EmotionStatistics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emotion.Core
+{
+    /// <summary>
+    /// Class for Emotion Statistics
+    /// </summary>
+    public class EmotionStatistics
+    {
+        #region Variables & Properties
+
+        /// <summary>
+        /// Total number of Emotion Calls
+        /// </summary>
+        public int totalEmotionCalls { get; set; }
+
+        /// <summary>
+        /// Total List of Emotion Calls Statistics
+        /// </summary>
+        public List<EmotionCallStatistics> totalEmotionCallsStatistics { get; set; }
+
+        /// <summary>
+        /// Mean of Emotion Calls per day
+        /// </summary>
+        public int emotionCallsPerDay { get; set; }
+
+        /// <summary>
+        /// Mean of Faces per Call
+        /// </summary>
+        public float facesPerCall { get; set; }
+
+        /// <summary>
+        /// Number of Detected Faces per Emotion
+        /// </summary>
+        public Tuple<String, int> detectionPerEmotion { get; set; }
+
+        #endregion
+
+        #region Calculs
+
+        #endregion
+    }
+}
---
+++ Emotion.forms/Emotion.forms/Emotion.forms/Statistics/TextAnalysisStatistics.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emotion.Core
+{ 
+    /// <summary>
+    /// Class for Text Analysis Statistics
+    /// </summary>
+    public class TextAnalysisStatistics
+    {
+        #region Variables & Properties
+
+        /// <summary>
+        /// Total number of Text Analysis Calls
+        /// </summary>
+        public int totalTextAnalysisCalls { get; set; }
+
+        /// <summary>
+        /// Total List of Text Analysis Calls Statistics
+        /// </summary>
+        public List<TextAnalysisCallStatistics> totalTextAnalysisCallsStatistics { get; set; }
+
+        /// <summary>
+        /// Mean of Text Analytics Calls per day
+        /// </summary>
+        public int textAnalyticsCallsPerDay { get; set; }
+
+        /// <summary>
+        /// Number of Detected phrases per Language
+        /// </summary>
+        public Tuple<String, int> detectionPerLanguage { get; set; }
+
+        #endregion
+
+        #region Calculs
+
+        #endregion
+
+    }
+}
